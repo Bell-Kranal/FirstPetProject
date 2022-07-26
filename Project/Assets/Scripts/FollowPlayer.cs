d7ff68176ecf6f1b3bf07065db073b2bc7511280
Бладыко Данила Евгеньевич <54392986+Bell-Kranal@users.noreply.github.com>
2022-07-26 21:33:12 +0300

    first commit
    This is a first my prototye game

---
+++ Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] private Transform _playerTransform;
+
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = _playerTransform.position + _offset;
+    }
+}
---
+++ Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _turnSpeed;
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        transform.Rotate(Vector3.up, _turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
+    }
+}
